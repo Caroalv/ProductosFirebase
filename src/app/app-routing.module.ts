8bf4129330ffa5a7ba7e44dd34a1749394b69878
Caroalv <alvaradocarolinad2002@gmail.com>
2024-04-11 14:13:13 -0600

    se agrego la paginacion y el pdf

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,8 @@ const routes: Routes = [
   {
     path: 'main',
     loadChildren: () => import('./pages/main/main.module').then( m => m.MainPageModule), canActivate:[AuthGuard]
-  },
+  }
+
 ];
 
 @NgModule({
--- src/app/pages/main/home/home.page.ts
+++ src/app/pages/main/home/home.page.ts
@@ -5,6 +5,13 @@ import { FirebaseService } from 'src/app/services/firebase.service';
 import { UtilsService } from 'src/app/services/utils.service';
 import { AddUpdateProductComponent } from 'src/app/shared/components/add-update-product/add-update-product.component';
 import { orderBy } from 'firebase/firestore';
+import * as pdfMake from 'pdfmake/build/pdfmake'; // Importa pdfmake
+import * as pdfFonts from 'pdfmake/build/vfs_fonts'; // Importa vfs_fonts
+(pdfMake as any).vfs = pdfFonts.pdfMake.vfs; // Configura vfs para pdfmake
+import { TDocumentDefinitions } from 'pdfmake/interfaces';
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+
 
 @Component({
   selector: 'app-home',
@@ -12,15 +19,96 @@ import { orderBy } from 'firebase/firestore';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-
+  productsCollection: AngularFirestoreCollection<Product>; // Declaración de la variable
   firebaseSvc = inject(FirebaseService);
   utilsSvc = inject(UtilsService)
 
 
 products: Product[] = [];
 loading: boolean = false;
+
+constructor(private firestore: AngularFirestore) {}
+
   ngOnInit() {
+    this.productsCollection = this.firestore.collection<Product>('users/{uid}/products');
+
   }
+  
+  loadData(event: any) {
+    setTimeout(() => {
+      let lastProduct = this.products[this.products.length - 1];
+      if (!lastProduct) {
+        // No hay productos cargados aún, no podemos cargar más
+        event.target.disabled = true;
+        event.target.complete();
+        return;
+      }
+  
+      let query = this.productsCollection.ref
+        .orderBy('soldUnits', 'desc')
+        .startAfter(lastProduct)
+        .limit(10); // Ajusta el número de productos a cargar
+  
+      query.get().then((querySnapshot) => {
+        if (!querySnapshot.empty) {
+          let newProducts = querySnapshot.docs.map(doc => doc.data() as Product);
+          this.products = this.products.concat(newProducts);
+          this.loading = false;
+        } else {
+          // No hay más productos para cargar, desactivamos el infinite scroll
+          event.target.disabled = true;
+        }
+  
+        event.target.complete();
+      }).catch((error) => {
+        console.error('Error al cargar más datos:', error);
+        event.target.complete();
+      });
+    }, 1000);
+  }
+  
+
+  generatePDF() {
+  let docDefinition: TDocumentDefinitions = {
+    content: [
+      {
+        text: 'Productos',
+        style: 'header'
+      },
+      {
+        table: {
+          headerRows: 1,
+          widths: [20, '*', 'auto', 'auto'],
+          body: [
+            [{ text: 'N', style: 'tableHeader' }, 'Nombre', 'Precio', 'Unidades Vendidas'],
+            ...this.products.map((product, index) => [index + 1, product.name, product.price.toString(), product.soldUnits.toString()])
+          ]
+        }
+      }
+    ],
+    styles: {
+      header: {
+        fontSize: 18,
+        bold: true,
+        margin: [0, 0, 0, 10]
+      },
+      tableHeader: {
+        bold: true,
+        fontSize: 13,
+        color: 'black'
+      }
+    }
+  };
+
+  // Utiliza pdfMake para generar el PDF
+  let pdfDoc = pdfMake.createPdf(docDefinition);
+
+  // Descarga el PDF en lugar de abrirlo en una nueva pestaña
+  pdfDoc.download('productos.pdf');
+}
+
+  
+  
 
   //============= Cerrar Sesion ==========
 
